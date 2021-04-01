ab96a1416f8f1fe83fde57725dd4b46a4fbe2248
andrei <losev_andrei@bk.ru>
2021-04-02 00:15:05 +0300

    express.local

--- back/build/app/models.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.StrKeys = exports.NumKeys = void 0;
-exports.NumKeys = [
-    'id', 'No', 'validation_cost', 'next_verification_date', 'next_verification_date', 'actual',
-];
-exports.StrKeys = [
-    'doc_title', 'manufacturer', 'validation_place', 'k_v_a', 'no_certificate', 'notes',
-];
